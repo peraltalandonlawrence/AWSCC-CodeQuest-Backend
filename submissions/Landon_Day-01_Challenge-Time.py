0e13ab786606e0e0da700be2af2d53306f4dcaea
Landon Lawrence A. Peralta <150335823+peraltalandonlawrence@users.noreply.github.com>
2023-11-21 05:40:46 +0000

    Day 1

---
+++ submissions/Landon_Day-01_Challenge-Time.py
@@ -0,0 +1,6 @@
+your_name = "Landon Lawrence A. Peralta"
+name_of_department = "Department of Software and Web Development"
+about_you = "I am a sporty person"
+
+print(f"Hello, I am {your_name}. I love {name_of_department} and I love being in the Backend even more!")
+print(f"One thing about me is that {about_you}.")
