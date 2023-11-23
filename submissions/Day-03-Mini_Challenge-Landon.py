02accdb8b85bf2719c3cc2e7bec388b8be4c1c55
Landon Lawrence A. Peralta <150335823+peraltalandonlawrence@users.noreply.github.com>
2023-11-23 23:47:32 +0800

    Delete submissions/Day-03-Mini_Challenge-Landon.py

--- submissions/Day-03-Mini_Challenge-Landon.py
+++
@@ -1,8 +0,0 @@
-num = int(input("Enter a number: "))
-
-if num > 0:
-    print (f"{num} is a Positive Number!")
-elif num < 0:
-    print(f"{num} is a Negative Number!")
-else:
-    print("That is a Zero!")
