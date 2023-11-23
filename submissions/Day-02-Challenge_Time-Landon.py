03b748462384826db1156bd5ae7685c91ba8bdd1
Landon Lawrence <peraltalandonlawrence@gmail.com>
2023-11-23 22:38:14 +0800

    Day 2 - Challenge Time

---
+++ submissions/Day-02-Challenge_Time-Landon.py
@@ -0,0 +1,7 @@
+fnum = int(input ("Enter the first number: "))
+snum = int(input ("Enter the second number: "))
+tnum = int(input ("Enter the third number: "))
+
+sum = (fnum) + (snum) + (tnum)
+
+print(f"\nSum: {sum}")
