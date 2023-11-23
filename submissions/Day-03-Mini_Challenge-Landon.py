3e793560b8ff8cfe8a570be8ab61e553d02f2133
Landon Lawrence <peraltalandonlawrence@gmail.com>
2023-11-23 23:08:52 +0800

    Day 3 - Mini Challenge

---
+++ submissions/Day-03-Mini_Challenge-Landon.py
@@ -0,0 +1,8 @@
+num = int(input("Enter a number: "))
+
+if num == 0:
+    print("That is a Zero!")
+elif num > 0:
+    print (f"{num} is a Positive Number!")
+else:
+    print(f"{num} is a Negative Number!")
